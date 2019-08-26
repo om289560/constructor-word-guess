96b2cb0d34765372f518c2fa0bcbfcba7407e471
Oscar Miranda <omiranda0058@gmail.com>
2019-08-25 21:19:36 -0500

    Wrote some code for letter.js.

--- word.js
+++ word.js
@@ -1,1 +1,12 @@
+var letter = require("./letter.js");
 
+function Word(answer) {
+    this.objArray = [];
+
+    for (var i = 0; i < answer.length; i++) {
+        var letter= new letter(answer[i]);
+        this.objArray.push(letter);
+    }
+
+    this.
+}

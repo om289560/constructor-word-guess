63550582deca1c728dc370687eb4f8f3be1780df
Oscar Miranda <omiranda0058@gmail.com>
2019-08-25 21:36:26 -0500

    Completed word.js code.

--- word.js
+++ word.js
@@ -8,5 +8,19 @@ function Word(answer) {
         this.objArray.push(letter);
     }
 
-    this.
+    this.log = function() {
+        answerLog = "";
+        for (var i = 0; i < this.objArray.length; i++) {
+            answer += this.objArray[i] + " ";    
+        }
+        console.log(answerLog + "\n============================\n");
+    };
+
+    this.userGuess = function(input) {
+        for (var i = 0; i < this.objArray.length; i++) {
+            this.objArray[i].guess(input);
+        }
+    };
 }
+
+module.exports = word;
